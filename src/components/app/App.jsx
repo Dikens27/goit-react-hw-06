a601eb184fdb1806667921abf534b11631aa998b
Dikens27 <kar.karovich321@gmail.com>
2024-03-21 19:41:39 +0200

    final

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,26 +1,9 @@
 import css from './App.module.css';
-import { useState, useEffect } from 'react';
 import ContactForm from '../contactForm/ContactForm';
 import SearchBox from '../searchBox/SearchBox';
 import ContactList from '../contactList/ContactList';
-import initialContacts from '../../contacts.json';
-
-const getInitialContacts = () => {
-  const savedContacts = localStorage.getItem('saved-contacts');
-  if (savedContacts) {
-    return JSON.parse(savedContacts);
-  } else {
-    return initialContacts;
-  }
-};
 
 export default function App() {
-  const [contacts] = useState(getInitialContacts);
-
-  useEffect(() => {
-    localStorage.setItem('saved-contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App.jsx';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
